3e410fa62fb3d5994bc46ec6c1bb1d116fab9069
Bobby Walker <BobbyWalker@users.noreply.github.com>
2022-01-24 23:52:40 -0600

    Added comments to the project

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {FormatQuote, Twitter} from "@mui/icons-material";
 import {useEffect, useState} from "react";
 import QuoteDataHook from "./QuoteData.hook";
 
+// These are styles applied to the buttons on the page.
 const buttonSX = {
     cursor: 'pointer',
     fontSize: '1.2rem',
@@ -15,25 +16,48 @@ const buttonSX = {
     padding: '0.5rem 1.8rem',
 }
 
+// The entry point to the application,
+// technically index.tsx is the entry
+// point, but it simply sets up React
+// and passes this component as the
+// primary object -- hence this is
+// the entry point.
 const App = () => {
     // Theme and media query to handle mobile differences
+    // the single difference here is the font size is
+    // slightly smaller on mobile devices
+    // useTheme retrieves a theme object
+    // with MUI defaults
+    // useMediaQuery attempts to determine if the
+    // screen size is smaller than their 'md' breakpoint
     const theme = useTheme()
     const matches = useMediaQuery(theme.breakpoints.down('md'))
 
     // Dynamically set to compensate for longer quotes, 2.75 or 2.00
     const [sFontSize, setFontSize] = useState('2.75rem')
+    // This state object exists when we want the quotes hook to return
+    // a new quote
     const [sNewQuote, setNewQuote] = useState<boolean|null>(null)
 
+    // The data hook that retrieves the quotes from the API and returns
+    // a random quote from that data set.
     const hQuotes = QuoteDataHook(sNewQuote)
 
     useEffect(() => {
         // Slightly smaller font for mobile devices.
+        // matches is based on the theme and media query
         if(matches)
             setFontSize('2.5rem')
     }, [matches])
 
     useEffect(() => {
+        // This effect runs when the hQuotes data changes
+        // OR when the newQuote value changes
+        // Set the newQuote value to null, as the
+        // quote data just changed.
         setNewQuote(null)
+        // Alter the font sized based on the length of the
+        // quote.  50 is some arbitrary number
         if(hQuotes.text.length >= 50)
             setFontSize('2rem')
         else
--- src/QuoteData.hook.ts
+++ src/QuoteData.hook.ts
@@ -1,30 +1,45 @@
 import {useEffect, useState} from "react";
 
+// The interface for a quote, I use the I designator
+// because it's a holdover from dabbling with C#
 export interface IQuote {
     text: string
     author: string
 }
 
+// This method actually fetches the data from an API
+// and converts it to a JSON object, then returns
+// the results
 const fetchQuotes = async (): Promise<IQuote[]> => {
     const quotes: IQuote[] = []
     try {
         const results = await fetch('https://type.fit/api/quotes')
-        const sayings = await results.text()
-        return JSON.parse(sayings)
+        return await results.json()
     } catch (error: any) {
         console.error('Error somewhere...', error.message)
         return quotes
     }
 }
 
+// This hook is responsible for managing the data for quotes.
+// It could be used from anywhere within the application and
+// provide the same data to multiple components.
 const QuoteDataHook = (nextQuote: boolean|null = null) => {
+    // We use state to hold a copy of the data
     const [sData, setData] = useState<IQuote[]>([])
+    // This state holds the current quote information
     const [sQuote, setQuote] = useState<IQuote>({
         text: `What you are is what you have been. What
         you'll be is what you do now.`,
         author: `Buddha`
     })
 
+    // This effect runs once when the hook is created
+    // and calls the fetchQuotes method to get the
+    // data.  Once it is returned, a random quote
+    // is selected from the list to be stored in
+    // sQuote, then sData is set with sQuote sliced
+    // out.
     useEffect(() => {
             // Populate quotes from the api...
         fetchQuotes()
@@ -38,6 +53,9 @@ const QuoteDataHook = (nextQuote: boolean|null = null) => {
             })
     }, [])
 
+    // This effect function runs when there is a change
+    // to nextQuote.  It updates sData with a new random
+    // quote and then removes the quote from sData
     useEffect(() => {
         if(nextQuote) {
             // Get the next quote from the array
@@ -50,6 +68,8 @@ const QuoteDataHook = (nextQuote: boolean|null = null) => {
         }
     }, [nextQuote, sData])
 
+    // Return a new IQuote to any components that have
+    // called this hook.
     return sQuote
 }
 
